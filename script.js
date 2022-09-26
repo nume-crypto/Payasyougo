cf7be9e06197e74f3664fa9713d6f7ac3d5d0443
gauthamm17 <114230705+gauthamm17@users.noreply.github.com>
2022-09-26 19:15:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+const date = new Date();
+const n = date.toDateString();
+const time = date.toLocaleTimeString();
+console.log('Date: ' + n);
+document.getElementById("showDate").innerHTML=n;
+console.log('Time: ' + time);
+
+$('.post-wrapper').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: $('.next'),
+    prevArrow: $('.prev'),
+
+  });
+
+  function updateProgressBar(ProgressBar, value){
+      ProgressBar.querySelector(".progress__fill").style.width = '${value}%';
+      ProgressBar.querySelector(".progress__text").textContent = '${value}%';
+  }
+//To show the unlock part of the page on scrolling
+$(document).scroll(function () {
+  var y = $(this).scrollTop();
+  if (y > 50) {
+      $('.container').fadeIn(1000);
+      $('#my').style.display = 'none';
+  } else {
+      $('.container').fadeOut();
+      $('.#my').style.display = 'block';
+  }
+
+});
+/*document.getElementById("button1").addEventListener("click", myFunction);*/
+//To increment NumePay wallet when user clicks on the button
+
+var x=0;
+document.getElementById("myBtn").addEventListener("click", showcontent);
+function showcontent() {
+    x=x+0.25;
+    document.getElementById("value").innerHTML = x +  "  BTC";
+    document.getElementById("pagebottom").style.display="none";
+}
+
+
+
+
+/*var element = document.getElementById("bottomMenu");
+  element.style.display = "none";
+  */
+
+//scroll the message box to the top offset of browser's scrool bar
+/*$(window).scroll(function()
+{
+  $('#message_box').animate({top:$(window).scrollTop()+"px" },{queue: false, duration: 350});
+});
+
+//when the close button at right corner of the message box is clicked
+$('#close_message').click(function()
+{
+  //the messagebox gets scrool down with top property and gets hidden with zero opacity
+  $('#message_box').animate({ top:"+=15px",opacity:0 }, "slow");
+});
+*/
+var hrefs = document.getElementsById("pagebottom");
+
+/*function openLink() {
+    var href = this.href;
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var tab = tabs[0];
+        chrome.tabs.update(tab.id, {url: "chrome-extension://flndlfhjfjmninbmdmcnallkdaibccal/popup.html"});
+    });
+}
+
+for (var i=0,a; a=hrefs[i]; ++i) {
+    hrefs[i].addEventListener('click', openLink);
+}
+*/ 
