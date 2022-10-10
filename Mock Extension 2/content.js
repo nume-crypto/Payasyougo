0f24f4bca5c27fc6c598468c08b860a895083991
gauthamm17 <gautham.madgula@numecrypto.com>
2022-10-10 21:23:26 +0530

    minor changes

--- Mock Extension 2/content.js
+++ Mock Extension 2/content.js
@@ -3,7 +3,7 @@ var articleTitle = document.querySelector("h2").textContent;
 console.log(articleTitle);
 
 var currentUrl = window.location.href;
-console.log("The current URL is"+currentUrl);
+console.log("The current URL is "+currentUrl);
 
 function displayExt() {
     chrome.runtime.sendMessage({message: 'buttonClicked'}, 
